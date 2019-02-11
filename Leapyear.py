eae5ed68dae89615e5bd7ea2734c4229b9376c98
Muthuanandavalli <47178001+Muthuanandavalli@users.noreply.github.com>
2019-02-11 21:56:23 +0530

    Update Leapyear.py

--- Leapyear.py
+++ Leapyear.py
@@ -1,7 +1,7 @@
-y=int(input())
-if y%4==0:
-    if y%100==0:
-        if y%400==0:
+a=int(input())
+if a%4==0:
+    if a%100==0:
+        if a%400==0:
             print("yes")
         else:
             print("no")
